d89df103122f322c7c9a2ce717f6350767fd5d8e
Jose Andrés Moncada <josemoncada87@hotmail.com>
2019-02-22 11:25:32 -0500

    updating

--- 9_diseno_eco/script_eco.js
+++ 9_diseno_eco/script_eco.js
@@ -9,102 +9,45 @@ title_team.innerText = "" + teams[parseInt(team)];
 var pistas = [false, false, false];
 showTip();
 
-var pistaA = document.getElementById("a");
-var pistaB = document.getElementById("b");
-var pistaC = document.getElementById("c");
+var feedbacks = document.getElementsByClassName("feedback_clave");
+var entradaPistas = document.getElementsByClassName("entradas_pistas_claves");
 
-pistaA.addEventListener("change", function () {
-    var feeback = document.getElementById("feedback_clave1");
-    if (pistaA.value === secretos[globalID][0]) {
-        feeback.innerHTML = " Correcto";
-        feeback.className = "ok_text_feedback";
-        pistas[0] = true;
-    } else {
-        feeback.innerHTML = " Oh Oh!! incorrecto";
-        feeback.className = "error_text_feedback";
-        pistas[0] = false;
-    }
-    showTip();
+for(let i = 0 ; i < 3 ; i++){
+    entradaPistas[i].addEventListener("change", function(){
+        validar(i);
+    });
+}
+
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+    validar(0);
+    validar(1);
+    validar(2);
 });
 
-pistaB.addEventListener("change", function () {
-    var feeback = document.getElementById("feedback_clave2");
-    if (pistaB.value === secretos[globalID][1]) {
-        feeback.innerHTML = " Correcto";
-        feeback.className = "ok_text_feedback";
-        pistas[1] = true;
+function validar(i) {
+    let actualFeedback = feedbacks[i];
+    if (entradaPistas[i].value === secretos[globalID][i]) {
+        actualFeedback.innerHTML = " Correcto";
+        actualFeedback.className = "ok_text_feedback feedback_clave";
+        pistas[i] = true;
     } else {
-        feeback.innerHTML = " Oh Oh!! incorrecto";
-        feeback.className = "error_text_feedback";
-        pistas[1] = false;
+        actualFeedback.innerHTML = " Oh Oh!! incorrecto";
+        actualFeedback.className = "error_text_feedback feedback_clave";
+        pistas[i] = false;
     }
     showTip();
-});
-
-
-pistaC.addEventListener("change", function () {
-    var feeback = document.getElementById("feedback_clave3");
-    if (pistaC.value === secretos[globalID][2]) {
-        feeback.innerHTML = " Correcto";
-        feeback.className = "ok_text_feedback";
-        pistas[2] = true;
-    } else {
-        feeback.innerHTML = " Oh Oh!! incorrecto";
-        feeback.className = "error_text_feedback";
-        pistas[2] = false;
-    }
-    showTip();
-});
+}
 
 function showTip() {
     var tipText = document.getElementById("url_next_container");
     if (pistas[0] === true && pistas[1] === true && pistas[2] === true) {
         tipText.className = "show_tip";
-        var ubicacionActual = rutas[parseInt(team)].indexOf(globalID);        
+        var ubicacionActual = rutas[parseInt(team)].indexOf(globalID);
         var indiceDestino = (ubicacionActual + 1);
         var destino = rutas[parseInt(team)][indiceDestino];
-        tipText.innerHTML = places[destino];        
+        tipText.innerHTML = places[destino];
     } else {
         tipText.className = "hidden_tip";
     }
 }
-
-
-var btn = document.getElementById("btn");
-btn.addEventListener("click", function () {
-
-    var feeback = document.getElementById("feedback_clave1");
-    if (pistaA.value === secretos[globalID][0]) {
-        feeback.innerHTML = " Correcto";
-        feeback.className = "ok_text_feedback";
-        pistas[0] = true;
-    } else {
-        feeback.innerHTML = " Oh Oh!! incorrecto";
-        feeback.className = "error_text_feedback";
-        pistas[0] = false;
-    }
-
-    var feeback = document.getElementById("feedback_clave2");
-    if (pistaB.value === secretos[globalID][1]) {
-        feeback.innerHTML = " Correcto";
-        feeback.className = "ok_text_feedback";
-        pistas[1] = true;
-    } else {
-        feeback.innerHTML = " Oh Oh!! incorrecto";
-        feeback.className = "error_text_feedback";
-        pistas[1] = false;
-    }
-
-    var feeback = document.getElementById("feedback_clave3");
-    if (pistaC.value === secretos[globalID][2]) {
-        feeback.innerHTML = " Correcto";
-        feeback.className = "ok_text_feedback";
-        pistas[2] = true;
-    } else {
-        feeback.innerHTML = " Oh Oh!! incorrecto";
-        feeback.className = "error_text_feedback";
-        pistas[2] = false;
-    }
-
-    showTip();
-});
--- helpers/global.js
+++ helpers/global.js
@@ -24,7 +24,14 @@ var teams = [
 /*8*/ "TDAH",
 /*9*/ "Vino Mora",
 /*10*/ "Zoo Prof/Pend",
-/*11*/ "Residuos Orgánicos"
+/*11*/ "Residuos Orgánicos",
+/*12*/ "E-commerce",
+/*13*/ "Manicuristas",
+/*14*/ "Ornitología",
+/*15*/ "Para-power lifting",
+/*16*/ "Procastinación",
+/*17*/ "Valle del Lili",
+/*18*/ "AR HITO"
 ];
 
 var secretos = [ 
@@ -52,5 +59,32 @@ var rutaGrupo8 = [0, 8, 9, 1, 2, 3, 4, 5, 6, 7, 10];
 var rutaGrupo9 = [0, 9, 1, 2, 3, 4, 5, 6, 7, 8, 10];
 var rutaGrupo10 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var rutaGrupo11 = [0, 2, 3, 4, 5, 6, 7, 8, 9, 1, 10];
+var rutaGrupo12 = [0, 3, 4, 5, 6, 7, 8, 9, 1, 2, 10];
+var rutaGrupo13 = [0, 4, 5, 6, 7, 8, 9, 1, 2, 3, 10];
+var rutaGrupo14 = [0, 5, 6, 7, 8, 9, 1, 2, 3, 4, 10];
+var rutaGrupo15 = [0, 6, 7, 8, 9, 1, 2, 3, 4, 5, 10];
+var rutaGrupo16 = [0, 7, 8, 9, 1, 2, 3, 4, 5, 6, 10];
+var rutaGrupo17 = [0, 8, 9, 1, 2, 3, 4, 5, 6, 7, 10];
+var rutaGrupo18 = [0, 9, 1, 2, 3, 4, 5, 6, 7, 8, 10];
 
-var rutas = [rutaGrupo0, rutaGrupo1, rutaGrupo2, rutaGrupo3, rutaGrupo4, rutaGrupo5, rutaGrupo6, rutaGrupo7, rutaGrupo8, rutaGrupo9, rutaGrupo10, rutaGrupo11];
+var rutas = [
+    rutaGrupo0,
+    rutaGrupo1,
+    rutaGrupo2,
+    rutaGrupo3,
+    rutaGrupo4,
+    rutaGrupo5,
+    rutaGrupo6,
+    rutaGrupo7,
+    rutaGrupo8,
+    rutaGrupo9,
+    rutaGrupo10,
+    rutaGrupo11,
+    rutaGrupo12,
+    rutaGrupo13,
+    rutaGrupo14,
+    rutaGrupo15,
+    rutaGrupo16,
+    rutaGrupo17,
+    rutaGrupo18,
+];
